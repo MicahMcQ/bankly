7b4e4661ff8ad3edce3a401ef25ab2f36060c58b
MicahMcQ <misemc7@gmail.com>
2023-11-08 23:36:31 -0500

    NODE @

---
+++ app.js
@@ -0,0 +1,49 @@
+/** Application for bank.ly */
+
+const express = require('express');
+const app = express();
+const ExpressError = require("./helpers/expressError");
+
+
+app.use(express.json());
+
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+
+/** 404 handler */
+
+app.use(function(req, res, next) {
+  const err = new ExpressError("Not Found", 404);
+
+  // pass the error to the next piece of middleware
+  return next(err);
+});
+
+/** general error handler */
+
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+
+//   return res.json({
+//     status: err.status,
+//     message: err.message
+//   });
+// });
+
+// **********I changed the general error handler to you ensure that there's always a response message, even if the error doesn't have one.********** 
+
+app.use(function(err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message || 'Internal Server Error';
+
+  res.status(status).json({
+    status: status,
+    message: message,
+  });
+});
+
+
+module.exports = app;
